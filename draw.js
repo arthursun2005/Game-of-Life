5924749db4697016c7b78056a76dbc36bec23422
Arthur Sun <arthur@HARRY-MAC.local>
2018-01-30 14:11:14 +1100

    rect fixed

--- draw.js
+++ draw.js
@@ -13,8 +13,12 @@ Draw.prototype.line = function(x1, y1, x2, y2, c, t) {
 Draw.prototype.rect = function(x, y, w, h, c1, c2) {
 	var d = this.space.getContext("2d");
 	d.beginPath();
-	d.fillStyle = c1;
 	d.strokeStyle = c2 || "#000000";
+	d.rect(x, y, w, h);
+	d.stroke();
+
+	d.beginPath();
+	d.fillStyle = c1;
 	d.fillRect(x, y, w, h);
 };
 Draw.prototype.arc = function(x1, y1, r, a1, a2, c) {
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 var LifeGrid = {
+	speed: 500,
 	life: "#00FF00",
 	dead: "#FFFFFF",
 	size: 5,
 	cells: [],
+	get: function(x,y){
+		try{
+			var v = this.cells[x+y*this.cx];
+		}catch(e){
+			throw e;
+			return;
+		}
+		return v;
+	},
+	change: function(x,y,v){
+	},
+	returnNeighbours: function(){
+	},
 	init: function(s){
-		s = s || 0;
+		if(!s) s=0;
 		this.c1 = document.getElementsByTagName("canvas")[s];
 		this.D = new Draw(this.c1);
 		this.c1.width = window.innerWidth;
@@ -12,21 +26,37 @@ var LifeGrid = {
 		this.ww = this.c1.width, this.hh = this.c1.height;
 		this.cx = Math.ceil(this.ww/this.size);
 		this.cy = Math.ceil(this.hh/this.size);
+		for(var i=0;i<this.cx*this.cy;i++){this.cells[i]=1;}
 	},
 	draw: function(){
-		for(var i=0;i<this.cells.length;i++){
+		for(var x=0;x<this.cx;x++){
+			for(var y=0;y<this.cy;y++){
+				var c = this.get(x,y) ? this.life : this.dead;
+				var c2 = "#666666";
+				this.D.rect(x*this.size,y*this.size,this.size,this.size,c,c2);
+			}
 		}
 	},
 	run: function(){
-
-		setTimeout(this.run,5);
+		this.D.rect(0,0,100,100,"#FFFFFF");
+		this.draw();
+		var run_handler = this.run.bind(this);
+		var speed = this.speed;
+		setTimeout(run_handler, speed);
 	},
-	changeSize: function(s){
-		if(!s) return;
-		if(typeof s == "number" && s>0){
+	changeSize: function(v){
+		var s = v;
+		if(s>0){
 			this.size = s;
 		}else{
-			alert("Enter a POSITVE NUMBER > 0");
+			alert("Enter a positve number for size");
+		}
+	},
+	changeSpeed: function(s){
+		if(s>0){
+			this.speed = s;
+		}else{
+			alert("Enter a positve number for speed");
 		}
 	}
 };
