fda8c19e0266e9d00a9a786d2b99ab8c3f20df0f
Arthur Sun <arthur@HARRY-MAC.local>
2018-01-31 15:51:28 +1100

    big fixed + new functions

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var LifeGrid = {
-	speed: 60,
+	speed: 20,
 	life: "#00FF00",
 	dead: "#000000",
-	size: 15,
+	size: 14,
 	cells: [],
-	controls: [false,'glider'],
+	controls: [false,'glider a'],
 	running: true,
 	get: function(x,y){
 		var v = this.cells[x%this.cx+(y%this.cy)*this.cx];
@@ -44,41 +44,66 @@ var LifeGrid = {
 	},
 	changeClick: function(name){
 		this.controls[1] = name;
+		console.log(name);
+	},
+	add: function(dx,dy,s,px,py){
+		for(var x=-1;x<=dx-2;x++){
+			for(var y=-1;y<=dy-2;y++){
+				var n = x+1+(y+1)*dx;
+				var hit = 0;
+				for(var i=0;i<s.length;i++){{if(n == s[i]) hit++;}}
+				if(hit>0){this.change(px+x,py+y,1);
+				}else{this.change(px+x,py+y,0);}
+			}
+		}
 	},
 	mouseclick: function(event){
 		var e = event || window.event;
 		var px = Math.floor(e.offsetX/this.size);
 		var py = Math.floor(e.offsetY/this.size);
-		if(this.controls[1] == 'glider'){
-			for(var x=-1;x<=1;x++){
-				for(var y=-1;y<=1;y++){
-					var n = x+1+y*3+3;
-					if(n == 2 || n == 3 || n == 5 || n == 7 || n == 8){this.change(px+x,py+y,1);
-					}else{this.change(px+x,py+y,0);}
-				}
-			}
-		}else if(this.controls[1] == 'lwss'){
-			for(var x=-2;x<=2;x++){
-				for(var y=-1;y<=2;y++){
-					var n = x+2+(y+1)*5;
-					if((n>0 && n<6) || n == 9 || n == 14 || n == 15 || n == 18){this.change(px+x,py+y,1);
-					}else{this.change(px+x,py+y,0);}
-				}
-			}
+		if(this.controls[1] == 'glider a'){
+			var s = [1,5,6,7,8];
+			this.add(3,3,s,px,py);
+		}else if(this.controls[1] == 'glider b'){
+			var s = [0,3,5,6,7];
+			this.add(3,3,s,px,py);
+		}else if(this.controls[1] == 'lwss r'){
+			var s = [1,2,3,4,5,9,14,15,18];
+			this.add(5,4,s,px,py);
+		}else if(this.controls[1] == 'lwss l'){
+			var s = [1,4,5,10,14,15,16,17,18];
+			this.add(5,4,s,px,py);
+		}else if(this.controls[1] == 'lwss u'){
+			var s = [0,1,2,4,7,8,12,17,19];
+			this.add(4,5,s,px,py);
+		}else if(this.controls[1] == 'lwss d'){
+			var s = [0,2,7,11,12,15,17,18,19];
+			this.add(4,5,s,px,py);
 		}else if(this.controls[1] == 'block'){
-			for(var x=0;x<=1;x++){
-				for(var y=0;y<=1;y++){
-					this.change(px+x,py+y,1);
-				}
-			}
+			for(var x=0;x<=1;x++){for(var y=0;y<=1;y++){this.change(px+x,py+y,1);}}
 		}else if(this.controls[1] == 'exploder'){
-			for(var x=-1;x<=1;x++){
-				for(var y=-1;y<=2;y++){
-					var n = x+1+(y+1)*3;
-					if(n == 1 || (n>2 && n<7) || n == 8 || n == 10){this.change(px+x,py+y,1);
-					}else{this.change(px+x,py+y,0);}
-				}
-			}
+			var s = [1,3,4,5,6,8,10];
+			this.add(3,4,s,px,py);
+		}else if(this.controls[1] == 'blinker'){
+			for(var x=-1;x<=-1;x++){for(var y=-1;y<=1;y++){this.change(px+x,py+y,1);}}
+		}else if(this.controls[1] == 'glider gun'){
+			var s = [
+				23,24,34,35,
+				22+38,24+38,34+38,35+38,
+				0+2*38,1+2*38,9+2*38,10+2*38,22+2*38,23+2*38,
+				0+3*38,1+3*38,8+3*38,10+3*38,
+				8+4*38,9+4*38,16+4*38,17+4*38,
+				16+5*38,18+5*38,
+				16+6*38,
+				35+7*38,36+7*38,
+				35+8*38,37+8*38,
+				35+9*38,
+
+				24+12*38,25+12*38,26+12*38,
+				24+13*38,
+				25+14*38,
+			];
+			this.add(38,15,s,px,py);
 		}
 	},
 	init: function(s){
